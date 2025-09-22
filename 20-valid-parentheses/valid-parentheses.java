9bbae93a7414634e3e1a9aac96d8f1dc2876d87a
Anam Fatima <2k22.cse.2212374@gmail.com>
2025-09-22 23:36:04 +0530

    Time: 4 ms (17.99%) | Memory: 42 MB (28.96%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
-                    return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (!stack.isEmpty()) {
+                char last = stack.peek();
+                if (isPair(last, cur)) {
+                    stack.pop();
+                    continue;
                 }
             }
+            stack.push(cur);
         }
-        return stack.isEmpty();
+
+        return stack.isEmpty();        
     }
+
+    private boolean isPair(char last, char cur) {
+        return (last == '(' && cur == ')') ||
+               (last == '{' && cur == '}') ||
+               (last == '[' && cur == ']');
+    }    
 }
